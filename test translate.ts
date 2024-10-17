fbee0aa9dd9cedc1126a994e0652e6071d372fc5
Jonas Gehman <107171291+BVEDod@users.noreply.github.com>
2024-10-17 23:54:23 +0800

    Update test translate.ts

--- test translate.ts
+++ test translate.ts
@@ -1,1 +1,23 @@
+const OPENAI_API_HOSTS = {
+    "translate": "translate.google.com",
+    "translate_api": "translate.googleapis.com",
+    "translate_pa": "translate-pa.googleapis.com"
+};
 
+Deno.serve(async (request) => {
+    const url = new URL(request.url);
+    const path = url.pathname.split('/')[1]; // 假设路径的第一个部分决定了使用哪个 API
+
+    // 根据路径选择 API 主机
+    const selectedHost = OPENAI_API_HOSTS[path] || OPENAI_API_HOSTS["translate"]; // 默认使用 translate.google.com
+
+    url.host = selectedHost;
+
+    const newRequest = new Request(url.toString(), {
+        headers: request.headers,
+        method: request.method,
+        body: request.body,
+        redirect: "follow",
+    });
+    return await fetch(newRequest);
+});
